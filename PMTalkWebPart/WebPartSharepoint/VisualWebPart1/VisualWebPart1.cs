243f3b56d9c2cc5e766d33fc4926011c198fb502
JensDel <jens.delannoy@hotmail.com>
2015-02-26 10:19:01 +0100

    Wil je normale namen aan de files geven aub

--- PMTalkWebPart/WebPartSharepoint/VisualWebPart1/VisualWebPart1.cs
+++ PMTalkWebPart/WebPartSharepoint/VisualWebPart1/VisualWebPart1.cs
@@ -6,13 +6,10 @@ using System.Web.UI.WebControls;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
 using System.Xml;
-<<<<<<< HEAD
 using System.Xml.Serialization;
 using System.IO;
 using System.Collections.Generic;
-=======
 using System.Web.UI.WebControls.WebParts;
->>>>>>> 5a556c5f8036729f4ad8e9439a9da7fe0c89990a
 
 namespace WebPartSharepoint.VisualWebPart1
 {
@@ -20,25 +17,15 @@ namespace WebPartSharepoint.VisualWebPart1
     /* Copyright by Maxime Gaveele 2015 Jon Bille Productions */
     
     [ToolboxItemAttribute(false)]
-    public class JonBille : WebPart
+    public class WebPart1 : WebPart
     {
         // Visual Studio might automatically update this path when you change the Visual Web Part project item.
         private const string _ascxPath = @"~/_CONTROLTEMPLATES/15/WebPartSharepoint/VisualWebPart1/VisualWebPart1UserControl.ascx";
-<<<<<<< HEAD
         private string fileContent;
-        public JonBille ()
+        public WebPart1 ()
 	{
         readFromFile("C:\\Development\\navSettings.xml");
 	}
-=======
->>>>>>> 5a556c5f8036729f4ad8e9439a9da7fe0c89990a
-
-        public VisualWebPart1()
-        {
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
         private void readFromFile(string filename)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(List<string>));
@@ -48,13 +35,6 @@ namespace WebPartSharepoint.VisualWebPart1
             {
                 List<string> dezerializedList = (List<string>)serializer.Deserialize(stream);
             }
-           
-=======
-        private void customEventHandler(object sender, WebPartEventArgs e)
-        {
-            
->>>>>>> 5a556c5f8036729f4ad8e9439a9da7fe0c89990a
->>>>>>> 6e13fffb13443a2fb4feaf92c01681d321bdb192
         }
 
         protected override void CreateChildControls()
