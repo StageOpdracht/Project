9091e24c8d9750aec32215c5c8a9e3e43084942a
JensDel <jens.delannoy@hotmail.com>
2015-02-26 11:13:34 +0100

    Lijst wordt toegevoegd aan de sharepoint

--- PMTalkWebPart/WebPartSharepoint/VisualWebPart1/VisualWebPart1.cs
+++ PMTalkWebPart/WebPartSharepoint/VisualWebPart1/VisualWebPart1.cs
@@ -5,11 +5,11 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
+using Microsoft.SharePoint.WebPartPages;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 using System.Collections.Generic;
-using System.Web.UI.WebControls.WebParts;
 
 namespace WebPartSharepoint.VisualWebPart1
 {
@@ -22,11 +22,30 @@ namespace WebPartSharepoint.VisualWebPart1
         // Visual Studio might automatically update this path when you change the Visual Web Part project item.
         private const string _ascxPath = @"~/_CONTROLTEMPLATES/15/WebPartSharepoint/VisualWebPart1/VisualWebPart1UserControl.ascx";
         private string fileContent;
+        private XsltListViewWebPart listView;
+        private List<Input> listItems;
 
         public WebPart1()
         {
+            listView = new XsltListViewWebPart();
+            //readFromFile("navSettings.xml");
+            FillList();
+        }
 
-            readFromFile("C:\\Development\\navSettings.xml");
+        private void FillList()
+        {
+            SPSite site = SPContext.Current.Site;
+            SPWeb web = site.OpenWeb();
+
+            //Create the list in sharepoint
+            listView.ID = "XsltListViewAppPromotedList";
+            listView.ListUrl = "Lists/TileList";
+            listView.IsIncluded = true;
+            listView.NoDefaultStyle = "TRUE";
+            listView.Title = "Images used in switcher";
+            listView.PageType = PAGETYPE.PAGE_NORMALVIEW;
+            listView.Default = "False";
+            listView.ViewContentTypeId = "0x";
         }
 
         private void readFromFile(string filename)
@@ -42,7 +61,9 @@ namespace WebPartSharepoint.VisualWebPart1
 
         protected override void CreateChildControls()
         {
-
+            base.CreateChildControls();
+            Controls.Add(listView);
         }
+
     }
 }
