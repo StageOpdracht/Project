47550661e85b7e8488d0e29da780292ccf54c4af
unknown <maxime.gaveele@SFDPMT03.pks.be>
2015-02-26 10:00:54 +0100

    inlezen van xml

--- PMTalkWebPart/WebPartSharepoint/VisualWebPart1/VisualWebPart1.cs
+++ PMTalkWebPart/WebPartSharepoint/VisualWebPart1/VisualWebPart1.cs
@@ -7,15 +7,25 @@ using System.Web.UI.WebControls.WebParts;
 using Microsoft.SharePoint;
 using Microsoft.SharePoint.WebControls;
 using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
 
 namespace WebPartSharepoint.VisualWebPart1
 {
+
+    /* Copyright by Maxime Gaveele 2015 Jon Bille Productions */
+    
     [ToolboxItemAttribute(false)]
-    public class VisualWebPart1 : WebPart
+    public class JonBille : WebPart
     {
         // Visual Studio might automatically update this path when you change the Visual Web Part project item.
         private const string _ascxPath = @"~/_CONTROLTEMPLATES/15/WebPartSharepoint/VisualWebPart1/VisualWebPart1UserControl.ascx";
         private string fileContent;
+        public JonBille ()
+	{
+        readFromFile("C:\\Development\\navSettings.xml");
+	}
 
         protected override void CreateChildControls()
         {
@@ -24,11 +34,16 @@ namespace WebPartSharepoint.VisualWebPart1
 
         }
 
-        private void readFromFile()
+        private void readFromFile(string filename)
         {
-            XmlTextReader reader = new XmlTextReader("");
+            XmlSerializer serializer = new XmlSerializer(typeof(List<string>));
 
-            fileContent = reader.ReadContentAsString();
+           
+            using (FileStream stream = File.OpenRead(filename))
+            {
+                List<string> dezerializedList = (List<string>)serializer.Deserialize(stream);
+            }
+           
         }
 
         public void AddListsToWebPart()
